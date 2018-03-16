fc15457fe662fb417fea608b1ec28a76ce4da851
Michal Srb <michal@redhat.com>
2018-03-16 08:56:34 +0100

    Access logger via current_app().logger

--- src/rest_api.py
+++ src/rest_api.py
@@ -40,7 +40,7 @@ def liveness():
 @app.route('/api/v1/stack-recommender', methods=['POST'])
 def stack_recommender():
     input_json = request.get_json()
-    app.logger.debug('stack-recommender/ request with payload: {p}'.format(p=input_json))
+    current_app.logger.debug('stack-recommender/ request with payload: {p}'.format(p=input_json))
 
     r = {'status': 'failure', 'external_request_id': None, 'message': 'Invalid request'}
     status = 400
@@ -65,7 +65,7 @@ def recommender():
     r = {'recommendation': 'failure', 'external_request_id': None}
     status = 400
     input_json = request.get_json()
-    app.logger.debug('recommender/ request with payload: {p}'.format(p=input_json))
+    current_app.logger.debug('recommender/ request with payload: {p}'.format(p=input_json))
 
     if input_json and 'external_request_id' in input_json and input_json['external_request_id']:
         try:
@@ -87,7 +87,7 @@ def recommender():
 def stack_aggregator():
     s = {'stack_aggregator': 'failure', 'external_request_id': None}
     input_json = request.get_json()
-    app.logger.debug('stack_aggregator/ request with payload: {p}'.format(p=input_json))
+    current_app.logger.debug('stack_aggregator/ request with payload: {p}'.format(p=input_json))
 
     if input_json and 'external_request_id' in input_json and input_json['external_request_id']:
         try:
