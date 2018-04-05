8462b2dbc01807f164a52baee243aaad01ae64ba
Geetika Batra <geetika791@gmail.com>
2018-04-05 23:16:36 +0530

    Changing jenkins env

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -6,7 +6,13 @@ set -ex
 
 # not needed for tests, but we can check that the image actually builds
 build_image
+prep(){
+	yum install -y epel-release
+	yum install -y gcc git python34-pip python34-requests httpd httpd-devel python34-devel
+	yum clean all
+}
+prep
 
 ./runtests.sh
 
-push_image
+
