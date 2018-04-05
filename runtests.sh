d182c61bcf66f225f776f3e85936787e9ec3d09e
Geetika Batra <geetika791@gmail.com>
2018-04-06 01:02:45 +0530

    updating test script to check jenkins

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@ gc() {
   docker-compose -f docker-compose.yml down -v || :
   exit $retval
 }
-trap gc EXIT SIGINT
+# trap gc EXIT SIGINT
 
 # Enter local-setup/ directory
 # Run local instances for: dynamodb, gremlin-websocket, gremlin-http
@@ -27,13 +27,14 @@ function prepare_venv() {
         # python34 which is in CentOS does not have virtualenv binary
         VIRTUALENV=`which virtualenv-3`
     fi
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt
+
 }
-prepare_venv
-pip3 install -r requirements.txt
-pip3 install git+https://github.com/fabric8-analytics/fabric8-analytics-worker.git@561636c
-pip3 install pytest
 
-PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` --cov=src/ --cov-report term-missing -vv tests/
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
-rm -rf venv/
+`which pip3` install git+https://github.com/fabric8-analytics/fabric8-analytics-worker.git@561636c
+
+cd tests
+PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` --cov=../src/ --cov-report term-missing -vv .
