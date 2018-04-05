71a5fbedc890923b47c7258493e7abdecc37346d
Geetika Batra <geetika791@gmail.com>
2018-04-05 14:26:44 +0530

    Testing livenes and rediness probe

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import json
 
 payload = {
     "result": [{
@@ -35,6 +36,30 @@ payload = {
 port = os.getenv("API_BACKBONE_SERVICE_PORT", "5000")
 
 url = "http://localhost:{port}/api/v1".format(port=port)
+
+def api_route_for(route):
+    """Construct an URL to the endpoint for given route."""
+    return '/api/v1/' + route
+
+
+def get_json_from_response(response):
+    """Decode JSON from response."""
+    return json.loads(response.data.decode('utf8'))
+
+
+def test_readiness_endpoint(client):
+    """Test the /api/v1/readiness endpoint."""
+    response = client.get(api_route_for("readiness"))
+    assert response.status_code == 200
+    json_data = get_json_from_response(response)
+    assert json_data == {}, "Empty JSON response expected"
+
+
+def test_liveness_endpoint(client):
+    """Test the /api/v1/liveness endpoint."""
+    response = client.get(api_route_for("liveness"))
+    assert response.status_code == 200
+    json_data = get_json_from_response(response)
 
 
 def test_stack_api_endpoint():
