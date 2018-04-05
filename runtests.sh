db55491a7d6fade6974201f4044dedb5dd8f246a
Geetika Batra <geetika791@gmail.com>
2018-04-05 15:07:35 +0530

    installs python3 for test job

--- runtests.sh
+++ runtests.sh
@@ -17,6 +17,9 @@ function start_backbone_service {
 }
 
 start_backbone_service
+yum install -y epel-release
+yum install -y gcc git python34-pip python34-requests httpd httpd-devel python34-devel
+yum clean all
 
 export PYTHONPATH=`pwd`/src
 echo "Create Virtualenv for Python deps ..."
@@ -26,12 +29,12 @@ function prepare_venv() {
         # python34 which is in CentOS does not have virtualenv binary
         VIRTUALENV=`which virtualenv-3`
     fi
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+    ${VIRTUALENV} -p `which python3` venv && source venv/bin/activate
 }
 prepare_venv
 pip3 install -r requirements.txt
 pip3 install git+https://github.com/fabric8-analytics/fabric8-analytics-worker.git@561636c
 
-python3 `which pytest` --cov=src/ --cov-report term-missing -vv tests/
+`which python3` `which pytest` --cov=src/ --cov-report term-missing -vv tests/
 
 rm -rf venv/
