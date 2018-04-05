379dcabb1713609057241523d1252cc999944bc2
Geetika Batra <geetika791@gmail.com>
2018-04-06 00:01:02 +0530

    Changed test_rest api

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -38,6 +38,11 @@ port = os.getenv("API_BACKBONE_SERVICE_PORT", "5000")
 url = "http://localhost:{port}/api/v1".format(port=port)
 
 
+def get_json_from_response(response):
+    """Decode JSON from response."""
+    return json.loads(response.data.decode('utf8'))
+
+
 def api_route_for(route):
     """Construct an URL to the endpoint for given route."""
     return '/api/v1/' + route
@@ -71,10 +76,12 @@ def test_stack_api_endpoint():
     assert(jsn['external_request_id'] is None)
 
 
-def test_recommendation_api_endpoint():
+def test_recommendation_api_endpoint(client):
     """Check the /recommender REST API endpoint."""
-    rec_resp = requests.post(url + "/recommender", json=payload)
-    jsn = rec_resp.json()
+    # rec_resp = requests.post(url + "/recommender", json=payload)
+    rec_resp = client.post(api_route_for("recommender"),
+                           data=json.dumps(payload), content_type='application/json')
+    jsn = get_json_from_response(rec_resp)
     assert(jsn['recommendation'] == 'failure')
     assert(jsn['external_request_id'] is None)
 
