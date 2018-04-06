042a6a3947b517cf6fc19d88712fc9e8482c8fa0
Geetika Batra <geetika791@gmail.com>
2018-04-06 18:14:58 +0530

    Tests (#51)
    * Set up pytest
    
    * Chnaging python version to  in test script
    
    * Testing livenes and rediness probe
    
    * Fixed linter issues
    
    * Adds .coverage file to gitignore
    
    * on exit, docker-compose.yaml missing error was found because of 'cd tests'
    
    * installs python3 for test job
    
    * Delete coverage file and add it to gitignore
    
    * Fixing pytest error in job
    
    * Installs pytest
    
    * Changing jenkins env
    
    * Changed test_rest api
    
    * Updated runtests.sh
    
    * updating test script
    
    * updating test script to check jenkins
    
    * upgrading pip
    
    * Updating cico scripts
    
    * Updating setup scripts

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@ gc() {
   docker-compose -f docker-compose.yml down -v || :
   exit $retval
 }
-trap gc EXIT SIGINT
+# trap gc EXIT SIGINT
 
 # Enter local-setup/ directory
 # Run local instances for: dynamodb, gremlin-websocket, gremlin-http
@@ -18,16 +18,26 @@ function start_backbone_service {
 
 start_backbone_service
 
+
 export PYTHONPATH=`pwd`/src
-
 echo "Create Virtualenv for Python deps ..."
-virtualenv venv
-source venv/bin/activate
+function prepare_venv() {
+    VIRTUALENV=`which virtualenv`
+    if [ $? -eq 1 ]; then
+        # python34 which is in CentOS does not have virtualenv binary
+        VIRTUALENV=`which virtualenv-3`
+    fi
 
-pip install -U pip
-pip install -r requirements.txt
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate 
+    pip install -U pip
+    python3 `which pip3` install -r requirements.txt
 
-cd tests
-PYTHONDONTWRITEBYTECODE=1 python `which pytest` --cov=../src/ --cov-report term-missing -vv .
+}
 
-rm -rf venv/
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+
+`which pip3` install git+https://github.com/fabric8-analytics/fabric8-analytics-worker.git@561636c
+`which pip3` install pytest
+`which pip3` install pytest-cov
+
+PYTHONDONTWRITEBYTECODE=1 python3 `which pytest` --cov=src/ --cov-report term-missing -vv tests/
--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import json
 
 payload = {
     "result": [{
@@ -37,6 +38,36 @@ port = os.getenv("API_BACKBONE_SERVICE_PORT", "5000")
 url = "http://localhost:{port}/api/v1".format(port=port)
 
 
+def get_json_from_response(response):
+    """Decode JSON from response."""
+    return json.loads(response.data.decode('utf8'))
+
+
+def api_route_for(route):
+    """Construct an URL to the endpoint for given route."""
+    return '/api/v1/' + route
+
+
+def get_json_from_response(response):
+    """Decode JSON from response."""
+    return json.loads(response.data.decode('utf8'))
+
+
+def test_readiness_endpoint(client):
+    """Test the /api/v1/readiness endpoint."""
+    response = client.get(api_route_for("readiness"))
+    assert response.status_code == 200
+    json_data = get_json_from_response(response)
+    assert json_data == {}, "Empty JSON response expected"
+
+
+def test_liveness_endpoint(client):
+    """Test the /api/v1/liveness endpoint."""
+    response = client.get(api_route_for("liveness"))
+    assert response.status_code == 200
+    json_data = get_json_from_response(response)
+
+
 def test_stack_api_endpoint():
     """Check the /stack_aggregator REST API endpoint."""
     stack_resp = requests.post(url + "/stack_aggregator", json=payload)
@@ -45,10 +76,12 @@ def test_stack_api_endpoint():
     assert(jsn['external_request_id'] is None)
 
 
-def test_recommendation_api_endpoint():
+def test_recommendation_api_endpoint(client):
     """Check the /recommender REST API endpoint."""
-    rec_resp = requests.post(url + "/recommender", json=payload)
-    jsn = rec_resp.json()
+    # rec_resp = requests.post(url + "/recommender", json=payload)
+    rec_resp = client.post(api_route_for("recommender"),
+                           data=json.dumps(payload), content_type='application/json')
+    jsn = get_json_from_response(rec_resp)
     assert(jsn['recommendation'] == 'failure')
     assert(jsn['external_request_id'] is None)
 
