8ee1ab7df2231c3d53bcee6e1957911b8aca6c68
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-20 10:10:17 +0200

    Added missing import

--- tests/test_stack_aggregator.py
+++ tests/test_stack_aggregator.py
@@ -1,5 +1,11 @@
 """Tests for the stack_aggregator module."""
 
+from unittest import TestCase
+from unittest.mock import *
+
+from src import stack_aggregator
+import json
+
 
 def test_validate_request_data():
     """Test the function validate_request_data."""
