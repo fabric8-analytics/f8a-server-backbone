447f98246a84513db97c912d7e789acc7e19ab5d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-20 10:25:09 +0200

    Test for extract_component_details function

--- tests/test_stack_aggregator.py
+++ tests/test_stack_aggregator.py
@@ -7,6 +7,25 @@ from src import stack_aggregator
 import json
 
 
-def test_validate_request_data():
+def test_extract_component_details():
     """Test the function validate_request_data."""
-    pass
+    with open("data/component_sequence.json", "r") as fin:
+        payload = json.load(fin)
+    results = stack_aggregator.extract_component_details(payload)
+    expected_keys = (
+        "ecosystem",
+        "name",
+        "version",
+        "licenses",
+        "security",
+        "osio_user_count",
+        "latest_version",
+        "github",
+        "code_metrics")
+    for expected_key in expected_keys:
+        assert expected_key in results, \
+            "Can not found the key '{key}' in result data structure".format(key=expected_key)
+
+
+if __name__ == '__main__':
+    test_extract_component_details()
