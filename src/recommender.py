442dc61b46bf4be4067434fe016d72037f877ddb
Ravindra Ratnawat <rratnawa@redhat.com>
2018-12-27 15:54:41 +0530

    fix(bug): transitive deps

--- src/recommender.py
+++ src/recommender.py
@@ -497,7 +497,7 @@ class RecommendationTask:
             temp_input_stack = {d["package"]: d["version"] for d in
                                 result.get("details", [])[0].get("_resolved")}
             for tdeps in result.get("details", [])[0].get("_resolved"):
-                temp_transitive_stack = [d for d in tdeps.get('deps', [])]
+                temp_transitive_stack = [d['package'] for d in tdeps.get('deps', [])]
                 logger.debug("transitive_stack {}".format(temp_transitive_stack))
                 transitive_stack.update(temp_transitive_stack)
             input_stack.update(temp_input_stack)
@@ -521,7 +521,7 @@ class RecommendationTask:
 
             insights_payload = {
                 'ecosystem': details['ecosystem'],
-                'transitive_stack': transitive_stack,
+                'transitive_stack': list(transitive_stack),
                 'unknown_packages_ratio_threshold':
                     float(os.environ.get('UNKNOWN_PACKAGES_THRESHOLD', 0.3)),
                 'package_list': new_arr,
