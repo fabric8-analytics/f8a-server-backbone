7cbdca6779714178f43b27369fb710be19c93599
animuk <animukhe@redhat.com>
2019-01-18 12:48:12 +0530

    testing 17

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 """The source code for f8a-server-backbone."""
-from f8a_worker.setup_celery import init_selinon
+# from f8a_worker.setup_celery import init_selinon
 
-init_selinon()
+# init_selinon()
