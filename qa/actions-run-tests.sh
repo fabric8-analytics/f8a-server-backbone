7ec12664c57f721da3bdd2b12c7d33f3d411f059
Meer Sawood <msawood@redhat.com>
2020-11-11 17:21:03 +0530

    Update and rename actions-setup.sh to actions-run-tests.sh

--- qa/actions-run-tests.sh
+++ qa/actions-run-tests.sh
@@ -14,3 +14,5 @@ export POSTGRESQL_USER="user"
 export POSTGRESQL_PASSWORD="password"
 export POSTGRESQL_DATABASE="dbname"
 export PGBOUNCER_SERVICE_HOST="bayesian-pgbouncer"
+
+py.test --cov=src/ --cov-report=xml --cov-fail-under=85 -vv tests/
