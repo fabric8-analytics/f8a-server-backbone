d1843b0b4388e61db1a69aeb81867e54c6d6ffc5
Deepak Sharma <deepshar@redhat.com>
2020-11-19 20:54:39 +0530

    review: normalized_packages
    Signed-off-by: Deepak Sharma <deepshar@redhat.com>

--- src/v2/normalized_packages.py
+++ src/v2/normalized_packages.py
@@ -3,12 +3,9 @@ import logging
 from collections import defaultdict
 from typing import List, Tuple, Dict, Set
 
-from werkzeug.exceptions import BadRequest
-
 from src.v2.models import Package, Ecosystem
 from f8a_utils.tree_generator import GolangDependencyTreeGenerator
 from f8a_utils.gh_utils import GithubUtils
-
 
 logger = logging.getLogger(__name__)
 
@@ -64,36 +61,21 @@ class GoNormalizedPackages(NormalizedPackages):
 
     def __init__(self, packages: List[Package], ecosystem: Ecosystem):
         """Create NormalizedPackages by removing all duplicates from packages."""
-        self._packages = packages
-        self._ecosystem = ecosystem
-        self._dependency_graph: Dict[Package, Set[Package]] = defaultdict(set)
-        self._modules = []
+        packages, self._modules = clean_and_get_pkgs(packages)
+        super().__init__(packages, ecosystem)
         self._version_map = {}
         gh = GithubUtils()
         self.pseudo = set()
         for package in packages:
             # clone without dependencies field
-            package.name, package.version, \
-                go_package_module = self.get_golang_metadata(package)
-            package_clone = Package(name=package.name, version=package.version)
             if gh.is_pseudo_version(package.version):
-                self._modules.append(go_package_module)
                 self._version_map[package.name] = package.version
-                self.pseudo.add(package_clone)
-            self._dependency_graph[package_clone] = self._dependency_graph[package_clone] or set()
+                self.pseudo.add(package)
             for trans_package in package.dependencies or []:
-                trans_package.name, trans_package.version, \
-                    trans_module = self.get_golang_metadata(trans_package)
-                trans_clone = Package(name=trans_package.name, version=trans_package.version)
                 if gh.is_pseudo_version(trans_package.version):
-                    self._modules.append(trans_module)
                     self._version_map[trans_package.name] = trans_package.version
-                    self.pseudo.add(package_clone)
-                self._dependency_graph[package].add(trans_clone)
+                    self.pseudo.add(trans_package)
         # unfold set of Package into flat set of Package
-        self._transtives: Set[Package] = {d for dep in self._dependency_graph.values() for d in dep}
-        self._directs = frozenset(self._dependency_graph.keys())
-        self._all = self._directs.union(self._transtives)
         self._all_except_pseudo = self._all.difference(self.pseudo)
 
     @property
@@ -111,11 +93,32 @@ class GoNormalizedPackages(NormalizedPackages):
         """Diff of all direct deps and pseudo deps."""
         return tuple(self._all_except_pseudo)
 
-    def get_golang_metadata(self, package) -> Tuple[str, str, str]:
-        """Clean Package Name, Pkg version & get Golang package_module and version_map."""
-        if "@" not in package.name:
-            raise BadRequest("Package name should be of format package@module.")
 
+def get_golang_metadata(package) -> Tuple[str, str]:
+    """Clean Package Name, Pkg version & get Golang package_module and version_map."""
+    package_module = package.name
+    package_name = None
+    if "@" in package.name:
         package_name, package_module = package.name.split("@")
-        _, package_version = GolangDependencyTreeGenerator.clean_version(package.version)
-        return package_name, package_version, package_module
+    if package_name is None:
+        package_name = package_module
+    return package_name, package_module
+
+
+def clean_and_get_pkgs(packages) -> Tuple[List[Package], List[str]]:
+    """Clean and get golang packages."""
+    all_packages: List[Package] = []
+    all_modules: List[str] = []
+    for direct in packages:
+        pkg_name, pkg_mod = get_golang_metadata(direct)
+        _, package_version = GolangDependencyTreeGenerator.clean_version(direct.version)
+        pkg = Package(name=pkg_name, version=package_version, dependencies=[])
+        all_modules.append(pkg_mod)
+        for trans_pkg in direct.dependencies or []:
+            trans_name, trans_mod = get_golang_metadata(trans_pkg)
+            _, trans_version = GolangDependencyTreeGenerator.clean_version(trans_pkg.version)
+            trans = Package(name=trans_name, version=trans_version)
+            all_modules.append(trans_mod)
+            pkg.dependencies.append(trans)
+        all_packages.append(pkg)
+    return all_packages, all_modules
