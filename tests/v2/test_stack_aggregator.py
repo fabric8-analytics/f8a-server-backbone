c492193b57b369cf3555d17c64cfd0d98644581d
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-11-24 19:45:32 +0530

    fix: URLEncode the package component

--- tests/v2/test_stack_aggregator.py
+++ tests/v2/test_stack_aggregator.py
@@ -382,6 +382,9 @@ class TestStackAggregator(TestCase):
         self.assertEqual(len(resp['result']['analyzed_dependencies'][0]
                              ['public_vulnerabilities']), 3)
 
+        print(resp['result']['analyzed_dependencies'][0]['url'])
+        self.assertEqual(resp['result']['analyzed_dependencies'][0]['url'],
+                         'https://snyk.io/vuln/golang:github.com%2Fgophish%2Fgophish%2Fcontrollers')
         self.assertEqual(resp['result']['analyzed_dependencies'][0]
                          ['public_vulnerabilities'][0]['cve_ids'], ['CVE-2020-24711'])
         self.assertEqual(resp['result']['analyzed_dependencies'][0]
