fabric8-analytics/f8a-server-backbone

commit 8991da0a689dea0e1d51e28044c9b3be92533d81

refactor: Simplify v2 recommender (#265)
This PR simplifies the v2 recommender implementation. Here is a flow,

- Call respective insight service
- Get details(github, latest_version,..) for the recommended packages(step-1) from graph
- Prepare response (version selection is based on latest_non_cve_version from Package node)

Note: Package will be dropped off from the recommendation incase if latest_non_cve_version is empty

Fixes https://issues.redhat.com/browse/APPAI-1694

tests/v2/test_recommender_api.py
@@ -1,0 +1,327 @@
+"""Tests for the REST API of the backbone service."""
+import json
+from unittest import mock
+
+from src.v2.models import RecommenderRequest, Package
+from tests.v2.data import (
+    npm_insights_response_for_request,
+    graph_package_response_for_npm_express_winston_moment,
+)
+
+
+def get_json_from_response(response):
+    """Decode JSON from response."""
+    response = json.loads(response.data.decode("utf8"))
+    assert response["result"]["_audit"] is not None
+    # delete _audit field as it has time value which is not assertable
+    del response["result"]["_audit"]
+    return response
+
+
+def test_invalid_request_payload(client):
+    """Test invalid request payload."""
+    response = client.post(
+        "/api/v2/recommender", data=json.dumps({}), content_type="application/json"
+    )
+    assert response.status_code == 500
+
+
+def test_golang_empty_response(client):
+    """Test golang ecosystem, it should return empty result."""
+    response = client.post(
+        "/api/v2/recommender",
+        data=json.dumps(
+            RecommenderRequest(
+                registration_status="",
+                external_request_id="",
+                ecosystem="golang",
+                manifest_file_path="",
+                packages=[],
+            ).dict()
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+    json_val = json.loads(response.data.decode("utf8"))
+    assert json_val == {}
+
+
+class _Response:
+    """Mock Response."""
+
+    def __init__(self, code, json):
+        self.status_code = code
+        self._json = json
+
+    def json(self):
+        """Json Response."""
+        return self._json
+
+    def raise_for_status(self):
+        if self.status_code != 200:
+            raise Exception("not 200")
+
+
+@mock.patch("src.v2.recommender.post_gremlin", return_value={"result": {"data": []}})
+@mock.patch("requests.Session.post", return_value=_Response(404, {}))
+def test_recommendation_response_with_insight_error(
+    _mock_insight_response, _mock_gremlin, client
+):
+    """Test golang ecosystem, it should return empty result."""
+    response = client.post(
+        "/api/v2/recommender?persist=false",
+        data=json.dumps(
+            RecommenderRequest(
+                registration_status="",
+                external_request_id="",
+                ecosystem="npm",
+                manifest_file_path="",
+                packages=[Package(name="request", version="2.88.2")],
+            ).dict()
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 500
+
+
+@mock.patch("src.v2.recommender.post_gremlin", return_value={"result": {"data": []}})
+@mock.patch("requests.Session.post", return_value=_Response(404, {}))
+def test_recommendation_response_with_empty_package_request(
+    _mock_insight_response, _mock_gremlin, client
+):
+    """Test with empty package list."""
+    response = client.post(
+        "/api/v2/recommender?persist=false",
+        data=json.dumps(
+            RecommenderRequest(
+                registration_status="FREETIER",
+                external_request_id="foo",
+                ecosystem="npm",
+                manifest_file_path="/foo.bar",
+                packages=[],
+            ).dict()
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 500
+
+
+@mock.patch("src.v2.recommender.post_gremlin", return_value={"result": {"data": []}})
+@mock.patch(
+    "requests.Session.post",
+    return_value=_Response(
+        200,
+        [
+            {
+                "companion_packages": [],
+                "ecosystem": "npm",
+                "missing_packages": ["blala"],
+                "package_to_topic_dict": {},
+            }
+        ],
+    ),
+)
+@mock.patch("src.v2.recommender.persist_data_in_db")
+def test_recommendation_response_with_empty_insight_response(
+    _mock_insight_response, _mock_gremlin, _mock_persist_db, client
+):
+    """Test with empty recommender response."""
+    response = client.post(
+        "/api/v2/recommender?persist=true",
+        data=json.dumps(
+            RecommenderRequest(
+                registration_status="FREETIER",
+                external_request_id="foo",
+                ecosystem="npm",
+                manifest_file_path="/foo.bar",
+                packages=[Package(name="blala", version="1.000")],
+            ).dict()
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+    response = get_json_from_response(response)
+    assert response == {
+        "external_request_id": "foo",
+        "recommendation": "success",
+        "result": {
+            "companion": [],
+            "external_request_id": "foo",
+            "manifest_file_path": "/foo.bar",
+            "manifest_name": None,
+            "recommendation_status": "success",
+            "registration_status": "FREETIER",
+            "usage_outliers": [],
+            "uuid": None,
+        },
+    }
+
+
+@mock.patch("src.v2.recommender.post_gremlin", return_value={"result": {"data": []}})
+@mock.patch(
+    "requests.Session.post",
+    return_value=_Response(
+        200,
+        npm_insights_response_for_request.DATA,
+    ),
+)
+def test_recommendation_response_with_empty_graph(
+    _mock_insight_response, _mock_gremlin, client
+):
+    """Test with empty graph response."""
+    response = client.post(
+        "/api/v2/recommender?persist=false",
+        data=json.dumps(
+            RecommenderRequest(
+                registration_status="FREETIER",
+                external_request_id="foo",
+                ecosystem="npm",
+                manifest_file_path="/foo.bar",
+                packages=[Package(name="request", version="2.88.2")],
+            ).dict()
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+    response = get_json_from_response(response)
+    assert response["result"] == {
+        "companion": [],
+        "external_request_id": "foo",
+        "manifest_file_path": "/foo.bar",
+        "manifest_name": None,
+        "recommendation_status": "success",
+        "registration_status": "FREETIER",
+        "usage_outliers": [],
+        "uuid": None,
+    }
+
+
+@mock.patch(
+    "src.v2.recommender.post_gremlin",
+    return_value=graph_package_response_for_npm_express_winston_moment.DATA,
+)
+@mock.patch(
+    "requests.Session.post",
+    return_value=_Response(
+        200,
+        npm_insights_response_for_request.DATA,
+    ),
+)
+def test_recommendation_response_with_2_packages_from_graph(
+    _mock_insight_response, _mock_gremlin, client
+):
+    """Test for happy scenario."""
+    response = client.post(
+        "/api/v2/recommender?persist=false",
+        data=json.dumps(
+            RecommenderRequest(
+                registration_status="FREETIER",
+                external_request_id="foo",
+                ecosystem="npm",
+                manifest_file_path="/foo.bar",
+                packages=[Package(name="request", version="2.88.2")],
+            ).dict()
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+    response = get_json_from_response(response)
+    assert response["result"] == {
+        "companion": [
+            {
+                "cooccurrence_count": 0,
+                "cooccurrence_probability": 52.50463667254613,
+                "dependencies": None,
+                "ecosystem": "npm",
+                "github": {
+                    "contributors": "238",
+                    "dependent_projects": "48988",
+                    "dependent_repos": "893994",
+                    "forks_count": "8571",
+                    "issues": {
+                        "month": {"closed": 18, "opened": 18},
+                        "year": {"closed": -1, "opened": -1},
+                    },
+                    "latest_release_duration": "2018-10-27 03:12:11",
+                    "open_issues_count": "155",
+                    "pull_requests": {
+                        "month": {"closed": 0, "opened": 3},
+                        "year": {"closed": 61, "opened": 75},
+                    },
+                    "size": "N/A",
+                    "stargazers_count": "51280",
+                    "total_releases": "291",
+                    "used_by": [
+                        {"name": "Automattic/mongoose", "stars": "14372"},
+                        {"name": "ReactTraining/react-router", "stars": "27232"},
+                        {"name": "angular/angular.js", "stars": "57749"},
+                        {"name": "angular/angular", "stars": "31547"},
+                        {"name": "facebook/jest", "stars": "14465"},
+                        {"name": "facebook/react", "stars": "84669"},
+                        {"name": "postcss/autoprefixer", "stars": "14331"},
+                        {"name": "reactjs/redux", "stars": "36922"},
+                        {"name": "socketio/socket.io", "stars": "38209"},
+                        {"name": "webpack/webpack", "stars": "35429"},
+                    ],
+                    "watchers": "1799",
+                },
+                "latest_version": "4.17.1",
+                "licenses": [],
+                "name": "express",
+                "topic_list": ["accepts"],
+                "url": "https://snyk.io/vuln/npm:express",
+                "version": "4.17.1",
+            },
+            {
+                "cooccurrence_count": 0,
+                "cooccurrence_probability": 51.04134789351682,
+                "dependencies": None,
+                "ecosystem": "npm",
+                "github": {
+                    "contributors": "30",
+                    "dependent_projects": "6413",
+                    "dependent_repos": "35152",
+                    "forks_count": "1129",
+                    "issues": {
+                        "month": {"closed": -1, "opened": -1},
+                        "year": {"closed": -1, "opened": -1},
+                    },
+                    "latest_release_duration": "2018-06-12 16:31:26",
+                    "open_issues_count": "236",
+                    "pull_requests": {
+                        "month": {"closed": -1, "opened": -1},
+                        "year": {"closed": -1, "opened": -1},
+                    },
+                    "size": "N/A",
+                    "stargazers_count": "12020",
+                    "total_releases": "73",
+                    "used_by": [
+                        {"name": "amir20/phantomjs-node", "stars": "2884"},
+                        {"name": "angular/angular.js", "stars": "56712"},
+                        {"name": "angular/angular", "stars": "26987"},
+                        {"name": "angular/material2", "stars": "10337"},
+                        {"name": "apidoc/apidoc", "stars": "4685"},
+                        {"name": "cyclejs/cyclejs", "stars": "7180"},
+                        {"name": "flatiron/prompt", "stars": "1142"},
+                        {"name": "foreverjs/forever", "stars": "9868"},
+                        {"name": "ionic-team/ionic-native", "stars": "1384"},
+                        {"name": "ionic-team/ionic", "stars": "30889"},
+                    ],
+                    "watchers": "224",
+                },
+                "latest_version": "3.2.1",
+                "licenses": [],
+                "name": "winston",
+                "topic_list": ["async"],
+                "url": "https://snyk.io/vuln/npm:winston",
+                "version": "3.2.1",
+            },
+        ],
+        "external_request_id": "foo",
+        "manifest_file_path": "/foo.bar",
+        "manifest_name": None,
+        "recommendation_status": "success",
+        "registration_status": "FREETIER",
+        "usage_outliers": [],
+        "uuid": None,
+    }

openshift/template.yaml
@@ -56,6 +56,8 @@ objects:
             value: f8a-pypi-insights
           - name: GOLANG_SERVICE_HOST
             value: f8a-golang-insights
+          - name: MAVEN_SERVICE_HOST
+            value: f8a-hpf-insights-maven
           - name: HPF_SERVICE_HOST
             value: f8a-hpf-insights
           - name: SERVICE_PORT

scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Start API backbone service with time out
-gunicorn --pythonpath /src/ -b 0.0.0.0:$API_BACKBONE_SERVICE_PORT -t $API_BACKBONE_SERVICE_TIMEOUT -k $CLASS_TYPE -w $NUMBER_WORKER_PROCESS rest_api:app
+gunicorn --pythonpath /src/ -b 0.0.0.0:$API_BACKBONE_SERVICE_PORT -t $API_BACKBONE_SERVICE_TIMEOUT -k $CLASS_TYPE -w $NUMBER_WORKER_PROCESS rest_api:app --reload --log-level $FLASK_LOGGING_LEVEL

src/rest_api.py
@@ -1,9 +1,10 @@
 """Implementation of the REST API for the backbone service."""
 
-import os
-import logging
 import flask
+import logging
+import os
 import time
+
 from f8a_worker.setup_celery import init_selinon
 from flask import Flask, request
 from flask_cors import CORS
@@ -11,32 +12,16 @@ from raven.contrib.flask import Sentry
 
 from src.recommender import RecommendationTask as RecommendationTaskV1
 from src.stack_aggregator import StackAggregator as StackAggregatorV1
+from src.utils import push_data, total_time_elapsed
 from src.v2.recommender import RecommendationTask as RecommendationTaskV2
 from src.v2.stack_aggregator import StackAggregator as StackAggregatorV2
-from src.utils import push_data, total_time_elapsed, get_time_delta
 
-
-def setup_logging(flask_app):
-    """Perform the setup of logging (file, log level) for this application."""
-    if not flask_app.debug:
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter(
-            '[%(asctime)s] %(levelname)s in %(pathname)s:%(lineno)d: %(message)s'))
-        log_level = os.environ.get('FLASK_LOGGING_LEVEL', logging.getLevelName(logging.WARNING))
-        handler.setLevel(log_level)
-
-        flask_app.logger.addHandler(handler)
-        flask_app.config['LOGGER_HANDLER_POLICY'] = 'never'
-        flask_app.logger.setLevel(logging.DEBUG)
-
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-setup_logging(app)
 CORS(app)
 SENTRY_DSN = os.environ.get("SENTRY_DSN", "")
 sentry = Sentry(app, dsn=SENTRY_DSN, logging=True, level=logging.ERROR)
-
-logger = logging.getLogger(__name__)
 
 init_selinon()
 
@@ -68,29 +53,25 @@ def _recommender(handler):
     }
 
     input_json = request.get_json()
-    if input_json and 'external_request_id' in input_json and input_json['external_request_id']:
-        external_request_id = input_json['external_request_id']
-        logger.info('%s recommender/ request with payload: %s', external_request_id, input_json)
-
-        try:
-            check_license = request.args.get('check_license', 'false') == 'true'
-            persist = request.args.get('persist', 'true') == 'true'
-            r = handler.execute(input_json, persist=persist,
-                                check_license=check_license)
-        except Exception as e:
-            r = {
-                'recommendation': 'unexpected error',
-                'external_request_id': input_json.get('external_request_id'),
-                'message': '%s' % e
-            }
-            metrics_payload['status_code'] = 400
-            logger.error('%s failed %s', external_request_id, r)
+    external_request_id = input_json['external_request_id']
+    logger.info('%s recommender/ request with payload: %s', external_request_id, input_json)
 
     try:
-        metrics_payload['value'] = get_time_delta(audit_data=r['result']['_audit'])
+        check_license = request.args.get('check_license', 'false') == 'true'
+        persist = request.args.get('persist', 'true') == 'true'
+        r = handler.execute(input_json, persist=persist,
+                            check_license=check_license)
+    except Exception as e:
+        r = {
+            'recommendation': 'unexpected error',
+            'external_request_id': input_json.get('external_request_id'),
+            'message': '%s' % e
+        }
+        metrics_payload['status_code'] = 400
+        metrics_payload['value'] = 0
         push_data(metrics_payload)
-    except KeyError:
-        pass
+        logger.error('%s failed %s', external_request_id, r)
+        raise e
 
     logger.info('%s took %0.2f seconds for _recommender',
                 external_request_id, time.time() - recommender_started_at)
@@ -114,38 +95,31 @@ def _stack_aggregator(handler):
         'status_code': 200
     }
 
-    if input_json and 'external_request_id' in input_json \
-            and input_json['external_request_id']:
-        external_request_id = input_json['external_request_id']
-        logger.info('%s stack_aggregator/ request with payload: %s',
-                    external_request_id, input_json)
+    external_request_id = input_json['external_request_id']
+    logger.info('%s stack_aggregator/ request with payload: %s',
+                external_request_id, input_json)
 
-        try:
-            persist = request.args.get('persist', 'true') == 'true'
-            s = handler.execute(input_json, persist=persist)
-            if s is not None and s.get('result') and s.get('result').get('_audit'):
-                # Creating and Pushing Total Metrics Data to Accumulator
-                metrics_payload['value'] = total_time_elapsed(
-                    sa_audit_data=s['result']['_audit'],
-                    external_request_id=input_json['external_request_id'])
-                push_data(metrics_payload)
-
-        except Exception as e:
-            s = {
-                'stack_aggregator': 'unexpected error',
-                'external_request_id': input_json.get('external_request_id'),
-                'message': '%s' % e
-            }
-            metrics_payload['status_code'] = 400
-            logger.error('%s failed %s', external_request_id, s)
-
-        try:
-            # Pushing Individual Metrics Data to Accumulator
-            metrics_payload['value'] = get_time_delta(audit_data=s['result']['_audit'])
-            metrics_payload['endpoint'] = request.endpoint
+    try:
+        persist = request.args.get('persist', 'true') == 'true'
+        s = handler.execute(input_json, persist=persist)
+        if s.get('result', {}).get('_audit'):
+            # Creating and Pushing Total Metrics Data to Accumulator
+            metrics_payload['value'] = total_time_elapsed(
+                sa_audit_data=s['result']['_audit'],
+                external_request_id=input_json['external_request_id'])
             push_data(metrics_payload)
-        except KeyError:
-            pass
+    except Exception as e:
+        s = {
+            'stack_aggregator': 'unexpected error',
+            'external_request_id': input_json.get('external_request_id'),
+            'message': '%s' % e
+        }
+        metrics_payload['status_code'] = 400
+        # Pushing Individual Metrics Data to Accumulator
+        metrics_payload['value'] = 0
+        push_data(metrics_payload)
+        logger.error('%s failed %s', external_request_id, s)
+        raise e
 
     logger.info('%s took %0.2f seconds for _stack_aggregators',
                 external_request_id, time.time() - stack_aggregator_started_at)

src/settings.py
@@ -5,11 +5,21 @@ from typing import Dict
 from pydantic import BaseSettings, HttpUrl
 
 
-# (fixme) Move all settings to read from here.
-class Settings(BaseSettings):
+class AggregatorSettings(BaseSettings):
     """Create Settings from env."""
 
-    snyk_package_url_format: HttpUrl = 'https://snyk.io/vuln/{ecosystem}:{package}'
-    snyk_signin_url: HttpUrl = 'https://snyk.io/login'
+    snyk_package_url_format: HttpUrl = "https://snyk.io/vuln/{ecosystem}:{package}"
+    snyk_signin_url: HttpUrl = "https://snyk.io/login"
     snyk_ecosystem_map: Dict[str, str] = {"pypi": "pip"}
     disable_unknown_package_flow: bool = False
+
+
+class RecommenderSettings(BaseSettings):
+    """Create Recommender Settings from env."""
+
+    chester_service_host: str = "notset"
+    pypi_service_host: str = "notset"
+    maven_service_host: str = "notset"
+    service_port: int = 6006
+    unknown_packages_threshold: float = 0.3
+    max_companion_packages: int = 5

src/stack_aggregator.py
@@ -12,7 +12,7 @@ from flask import current_app
 import requests
 import copy
 from collections import defaultdict
-from src.settings import Settings
+from src.settings import AggregatorSettings
 from src.utils import (select_latest_version, server_create_analysis, LICENSE_SCORING_URL_REST,
                        post_http_request, GREMLIN_SERVER_URL_REST, persist_data_in_db,
                        GREMLIN_QUERY_SIZE, format_date)
@@ -663,7 +663,7 @@ class StackAggregator:
                          'result': stack_data}
         # Ingestion of Unknown dependencies
         logger.info("Unknown ingestion flow process initiated.")
-        if Settings().disable_unknown_package_flow:
+        if AggregatorSettings().disable_unknown_package_flow:
             logger.warning('Skipping unknown flow %s', unknown_dep_list)
         else:
             try:

src/utils.py
@@ -324,21 +324,21 @@ def post_http_request(url: str, payload: Dict):
 
 def post_gremlin(query: str, bindings: Dict = None) -> Dict:
     """Post the given query and bindings to gremlin endpoint."""
+    payload = {
+        'gremlin': query,
+    }
+    if bindings:
+        payload['bindings'] = bindings
     try:
-        payload = {
-            'gremlin': query,
-        }
-        if bindings:
-            payload['bindings'] = bindings
         response = get_session_retry().post(url=GREMLIN_SERVER_URL_REST, json=payload)
         response.raise_for_status()
-        return response.json()
     except Exception as e:
         logger.error(traceback.format_exc())
-        logger.error(
-            "HTTP error {code}. Error retrieving data for {query}.".format(
-                code=response.status_code, query=payload))
+        logger.error("HTTP error %d. Error retrieving data for %s.",
+                     response.status_code, payload)
         raise GremlinExeception from e
+    else:
+        return response.json()
 
 
 def get_response_data(json_response, data_default):

commit 77459c038670d76341d5e8afd5e208dd1dd3d391

fix: Add empty recommendation entry for golang (#267)
This fixes the regression introduced by #265 by adding an empty entry for golang recommendation.

src/v2/recommender.py
@@ -170,16 +170,19 @@ class RecommendationTask:
         """Execute task."""
         started_at = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
         request = RecommenderRequest(**arguments)
-        if request.ecosystem == "golang":
-            logging.warning("Recommendation is yet to be implemented for golang")
-            return {}
-
-        normalized_packages = NormalizedPackages(request.packages, request.ecosystem)
-        insights_response = self._get_insights_response(normalized_packages)
+        if request.ecosystem != "golang":
+            normalized_packages = NormalizedPackages(
+                request.packages, request.ecosystem
+            )
+            insights_response = self._get_insights_response(normalized_packages)
+            companion = self._get_recommended_package_details(insights_response[0])
+        else:
+            companion = []
+            logging.warning("Recommendation is not yet implemented for golang")
 
         result = StackRecommendationResult(
             **arguments,
-            companion=self._get_recommended_package_details(insights_response[0]),
+            companion=companion,
         )
 
         recommendation = result.dict()

tests/v2/test_recommender_api.py
@@ -29,21 +29,30 @@ def test_invalid_request_payload(client):
 def test_golang_empty_response(client):
     """Test golang ecosystem, it should return empty result."""
     response = client.post(
-        "/api/v2/recommender",
+        "/api/v2/recommender?persist=false",
         data=json.dumps(
             RecommenderRequest(
-                registration_status="",
-                external_request_id="",
+                registration_status="REGISTERED",
+                external_request_id="foo",
                 ecosystem="golang",
-                manifest_file_path="",
+                manifest_file_path="/foo.bar",
                 packages=[],
             ).dict()
         ),
         content_type="application/json",
     )
     assert response.status_code == 200
-    json_val = json.loads(response.data.decode("utf8"))
-    assert json_val == {}
+    response = get_json_from_response(response)
+    assert response["result"] == {
+        "companion": [],
+        "external_request_id": "foo",
+        "manifest_file_path": "/foo.bar",
+        "manifest_name": None,
+        "recommendation_status": "success",
+        "registration_status": "REGISTERED",
+        "usage_outliers": [],
+        "uuid": None,
+    }
 
 
 class _Response: