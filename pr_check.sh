f521d399d3b429af123f67aa44cb4369bdac24a1
Anilkumar Prodduturi <aproddut@redhat.com>
2021-01-11 10:54:00 -0500

    removing pr_check as its done via github actions and making build_deploy executable

--- pr_check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -exv
-docker build  --no-cache \
-              --force-rm \
-              -t rhel-fabric8-analytics-f8a-server-backbone:latest  \
-              -f ./Dockerfile.rhel .
