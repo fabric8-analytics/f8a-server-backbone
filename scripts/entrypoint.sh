cb8433813c970c43495f3cef8940c9aaacf541f4
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2021-01-13 16:59:01 +0530

    fix: Preload gunicorn app for optimal resource usage (#273)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Start API backbone service with time out
-gunicorn --pythonpath /src/ -b 0.0.0.0:$API_BACKBONE_SERVICE_PORT -t $API_BACKBONE_SERVICE_TIMEOUT -k $CLASS_TYPE -w $NUMBER_WORKER_PROCESS rest_api:app --reload --log-level $FLASK_LOGGING_LEVEL
+gunicorn --pythonpath /src/ -b 0.0.0.0:$API_BACKBONE_SERVICE_PORT -c /src/conf/gunicorn_backbone.py rest_api:app --log-level $FLASK_LOGGING_LEVEL
---
+++ src/conf/gunicorn_backbone.py
@@ -0,0 +1,21 @@
+"""Gunicorn config."""
+
+import logging
+from src.settings import GUNICORN_SETTINGS
+
+workers = GUNICORN_SETTINGS.workers
+worker_class = GUNICORN_SETTINGS.worker_class
+timeout = GUNICORN_SETTINGS.timeout
+preload_app = GUNICORN_SETTINGS.preload
+reload = preload_app is not True
+
+
+def when_ready(server):  # noqa
+    """Log when worker is ready to serve."""
+    logger = logging.getLogger(__name__)
+    logger.info(
+        "Starting backbone gunicorn with %s workers %s worker class and preload %s",
+        workers,
+        worker_class,
+        preload_app,
+    )
