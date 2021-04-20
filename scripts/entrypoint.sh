992548fd3258a39fc9e5d8ca5a4290389176d7b5
Arunprasad Rajkumar <arajkuma@redhat.com>
2021-04-20 15:21:27 +0530

    fix: alter copy according to ubi default working dir
    Signed-off-by: Arunprasad Rajkumar <arajkuma@redhat.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Start API backbone service with time out
-gunicorn --pythonpath /src/ -b 0.0.0.0:$API_BACKBONE_SERVICE_PORT -c /src/conf/gunicorn_backbone.py rest_api:app --log-level $FLASK_LOGGING_LEVEL
+gunicorn -b 0.0.0.0:$API_BACKBONE_SERVICE_PORT -c src/conf/gunicorn_backbone.py src.rest_api:app --log-level $FLASK_LOGGING_LEVEL
